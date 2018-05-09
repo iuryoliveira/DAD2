0df9a612fe4d62113ac53e8ae02880c21634f67d
Iury Magalhaes <iurylei@hotmail.com>
2018-05-09 20:05:32 -0300

    Hanndler progressBar login

--- app/src/main/java/com/example/iury/gerenciadordeenergiaeltrica/Login.java
+++ app/src/main/java/com/example/iury/gerenciadordeenergiaeltrica/Login.java
@@ -2,17 +2,22 @@ package com.example.iury.gerenciadordeenergiaeltrica;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity{
     EditText login;
     EditText senha;
+    ProgressBar progresso;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,8 @@ public class Login extends AppCompatActivity{
         login = (EditText) findViewById(R.id.txtLogin);
         senha = (EditText) findViewById(R.id.txtSenha);
         login.requestFocus();
+
+        progresso = (ProgressBar) findViewById(R.id.progressLogin);
     }
 
     private boolean isEmpty(EditText etText) {
@@ -42,6 +49,29 @@ public class Login extends AppCompatActivity{
     }
 
     public void login(View view){
+
+        progresso.setVisibility(View.VISIBLE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(progressStatus < 100){
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progresso.setProgress(progressStatus);
+                        }
+                    });
+                    try{
+                        Thread.sleep(100);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        
         if(!isEmpty(login) && !isEmpty(senha)) {
             startActivity(new Intent(this, PerfilConsumo.class));
             finish();
